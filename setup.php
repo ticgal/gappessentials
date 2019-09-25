ae733e4c34ca042148d8410a0aa613cd6ccc4413
Xacobo Freire <39123808+xacobofg@users.noreply.github.com>
2019-09-25 19:07:40 +0200

    New version 1.1.0

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_GAPPESSENTIALS_VERSION', '1.0.0');
+define('PLUGIN_GAPPESSENTIALS_VERSION', '1.1.0');
 // Minimal GLPI version, inclusive
 define("PLUGIN_GAPPESSENTIALS_MIN_GLPI", "9.4.0");
 define("PLUGIN_GAPPESSENTIALS_MAX_GLPI", "9.5");
